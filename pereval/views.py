294564baa5e9c6837ca7af8a4bb27f39ec8654cd
RuslanF <voxvt@mail.ru>
2024-07-24 07:13:13 +0500

    добвалена возможность  получить одну запись по её id, отредактировать существующую запись, если она в статусе new и писок данных обо всех объектах, которые пользователь с почтой <email> отправил на сервер

--- pereval/views.py
+++ pereval/views.py
@@ -3,6 +3,8 @@ from rest_framework import viewsets, status, generics
 from rest_framework.response import Response
 from .serializers import *
 from .models import *
+from .models import Pereval
+from .serializers import PerevalSerializer
 
 
 class UserViewset(viewsets.ModelViewSet):
@@ -28,28 +30,58 @@ class ImageViewset(viewsets.ModelViewSet):
 class PerevalViewset(viewsets.ModelViewSet):
     queryset = Pereval.objects.all()
     serializer_class = PerevalSerializer
-    filterset_fields = ('user__email',)
+    filterset_fields = ['user__email']
 
     def create(self, request, *args, **kwargs):
         serializer = PerevalSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response({
-                'status': status.HTTP_200_OK,
-                'message': 'Успех',
-                'id': serializer.data['id']
-            })
-        if status.HTTP_400_BAD_REQUEST:
+                'status': status.HTTP_201_CREATED,
+                'message': None,
+                'id': serializer.data['id'],
+            }, status=status.HTTP_201_CREATED)
+        return Response({
+            'status': status.HTTP_400_BAD_REQUEST,
+            'message': serializer.errors,
+            'id': None,
+        }, status=status.HTTP_400_BAD_REQUEST)
+
+    def partial_update(self, request, *args, **kwargs):
+        pereval = self.get_object()
+        if pereval.status == 'new':
+
+            data = request.data.copy()
+            data.pop('user', None)
+
+            serializer = self.get_serializer(pereval, data=data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({
+                    'state': 1,
+                    'message': 'Запись успешно изменена'
+                }, status=status.HTTP_200_OK)
+            else:
+                return Response({
+                    'state': 0,
+                    'message': serializer.errors
+                }, status=status.HTTP_400_BAD_REQUEST)
+        else:
             return Response({
-                'status': status.HTTP_400_BAD_REQUEST,
-                'message': 'Bad Request - не хватает полей',
-                'id': None
-            })
-        if status.HTTP_500_INTERNAL_SERVER_ERROR:
-            return Response({
-                'status': status.HTTP_500_INTERNAL_SERVER_ERROR,
-                'message': 'Ошибка подключения к базе данных',
-                'id': None
-            })
+                'state': 0,
+                'message': f"Отклонено. Причина: {pereval.get_status_display()}"
+            }, status=status.HTTP_400_BAD_REQUEST)
 
 
+    def update(self, request, *args, **kwargs):
+        return Response({
+            'state': 0,
+            'message': 'Используйте метод partial_update для частичных обновлений'
+        }, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+
+    def destroy(self, request, *args, **kwargs):
+        return Response({
+            'state': 0,
+            'message': 'Удаление не разрешено'
+        }, status=status.HTTP_405_METHOD_NOT_ALLOWED)
--- pereval_project/urls.py
+++ pereval_project/urls.py
@@ -19,5 +19,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-]
+    ]
 
